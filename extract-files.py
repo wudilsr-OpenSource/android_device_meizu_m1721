1d4073c70f740f03115f51d58e2782b221f33c38
ZeeLog <Danjel.Zammit@gmail.com>
2025-01-19 22:23:05 +0100

    m1721: Remove libmmosal from extract-files lib_fixup entries

--- extract-files.py
+++ extract-files.py
@@ -33,7 +33,6 @@ lib_fixups: lib_fixups_user_type = {
     **lib_fixups,
     (
         'com.qualcomm.qti.dpm.api@1.0',
-        'libmmosal',
         'vendor.qti.imsrtpservice@3.0',
     ): lib_fixup_vendor_suffix,
     ('libwpa_client'): lib_fixup_remove,
