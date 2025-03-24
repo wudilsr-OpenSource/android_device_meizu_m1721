06cb34682c3feb1480dbc279b3cb489a0056f6ba
ZeeLog <Danjel.Zammit@gmail.com>
2025-03-24 18:30:47 +0100

    m1721: Opt out DeviceDiagnostics package

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -5,7 +5,7 @@ LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
 LOCAL_OVERRIDES_PACKAGES := \
-    NfcNci SecureElement DeviceAsWebcam
+    NfcNci SecureElement DeviceAsWebcam DeviceDiagnostics
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
