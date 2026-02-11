248c973dfc73bfa5afc33f746c803c8cfcada603
wudilsr <wudilsr@gmail.com>
2026-02-11 13:05:37 +0800

    m1721: Remove invalid boot image profile path (non-existent file)
    Remove reference to frameworks/base/boot/boot-image-profile-extra.txt from
    PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION since the file is missing,
    preventing build issues.

--- device.mk
+++ device.mk
@@ -376,8 +376,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
 PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := \
-    frameworks/base/boot/boot-image-profile.txt \
-    frameworks/base/boot/boot-image-profile-extra.txt
+    frameworks/base/boot/boot-image-profile.txt
 
 # Reduce system image size by limiting debug info
 PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
