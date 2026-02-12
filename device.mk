0bd126f33d71420aa285b606e14af25d30e783d0
kubersharma001 <kubersharma001@gmail.com>
2026-02-12 17:20:33 +0800

    m1721: Compile libldacBT_bco.so
    * E a2dp_vendor_ldac_decoder: A2DP_VendorLoadDecoderLdac: cannot open LDAC decoder library libldacBT_bco.so: dlopen failed: library "libldacBT_bco.so" not found

--- device.mk
+++ device.mk
@@ -102,6 +102,7 @@ PRODUCT_PACKAGES += \
     audio.primary.msm8953:32 \
     audio.r_submix.default \
     audio.usb.default \
+    libldacBT_bco \
     libaacwrapper \
     libaudio-resampler \
     libqcomvisualizer \
