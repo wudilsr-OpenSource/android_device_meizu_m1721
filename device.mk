d630c2dbb5da6d3e4f557ea1f944715099a6cae9
Wang Han <416810799@qq.com>
2026-02-13 21:04:21 +0800

    m1721: configs: Import gpfspath_oem_config.xml
    * Opened by libdrmfs.so. Add it to be safe better than sorry.
    
    Change-Id: I46f33fba4591750a3c932e15b2b80a3c071ee4dd

--- device.mk
+++ device.mk
@@ -192,6 +192,9 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.4.vendor \
     android.hardware.drm-service.clearkey
 
+PRODUCT_COPY_FILES += \
+        $(LOCAL_PATH)/configs/gpfspath_oem_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/gpfspath_oem_config.xml
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.meizu_m1721
