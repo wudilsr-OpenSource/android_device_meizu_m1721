e56f7e8d9db1c18ac0a08abea0806e4b0f0af155
wudilsr <wudilsr@gmail.com>
2026-02-14 21:12:39 +0800

    Revert "m1721: Disable the usage of ConfigStore."
    This reverts commit 76572c33941bfdadb2d63053237c8ba7236d490b.

--- device.mk
+++ device.mk
@@ -156,10 +156,6 @@ PRODUCT_COPY_FILES += \
     system/core/libprocessgroup/profiles/cgroups_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
     system/core/libprocessgroup/profiles/task_profiles_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
-# ConfigStore
-PRODUCT_PACKAGES += \
-    disable_configstore
-
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl:64 \
