b1d506bdcbc4b9f93544b83fa0d05585b357309c
wudilsr <wudilsr@gmail.com>
2026-02-18 12:54:52 +0800

    m1721: Remove libldacBT_bco

--- device.mk
+++ device.mk
@@ -97,7 +97,6 @@ PRODUCT_PACKAGES += \
     audio.primary.msm8953:32 \
     audio.r_submix.default \
     audio.usb.default \
-    libldacBT_bco \
     libaacwrapper \
     libaudio-resampler:32 \
     libqcomvisualizer:32 \
