ed3705abf23a352be157304269434d3700c2f3b7
daniml3 <danimoral1001@gmail.com>
2026-02-18 20:52:12 +0800

    m1721: Switch to SYSTEM_EXT_*_SEPOLICY_DIRS
    * BOARD_PLAT_*_SEPOLICY_DIR has been deprecated

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -180,6 +180,7 @@ VENDOR_SECURITY_PATCH := 2023-01-23
 # SELinux
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 
 # Treble
 PRODUCT_FULL_TREBLE_OVERRIDE := true
