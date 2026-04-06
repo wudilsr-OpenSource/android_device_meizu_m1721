ee1914e763bb50a690819efc52d78ccc4b86c41f
wudilsr <wudilsr@gmail.com>
2026-04-06 18:58:28 +0800

    m1721: Update blobs from FP3
    * from Fairphone/FP3/FP3-REL-6.A.023.1-gms-497e9bef-user

--- device.mk
+++ device.mk
@@ -257,7 +257,7 @@ PRODUCT_PACKAGES += \
     CarrierConfigOverlay \
     ims-ext-common \
     ims_ext_common.xml \
-    libgui_shim
+    libgui_shim:64
 
 # Inherit several Android Go Configurations(Beneficial for everyone, even on non-Go devices)
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
