72f993fc861f80cf93576e83dc4a30895a7ffaf9
iusmac <iusico.maxim@libero.it>
2026-04-06 11:20:01 +0800

    m1721: Explicitly disable Lineage Health feature
    LOS devs for a strange reason to me have enabled health feature that is
    optional by design with assumption that everyone will include the rest,
    i.e the 'vendor.lineage.health-service.default' package in their DTs +
    sysfs node permission setup.
    Currently, this randomly crashes the Settings app due to missing Health
    HAL service. It may even break displaying of battery stats.
    
    Since we're shipping our implementation of charging control, we have
    nothing else to do but do exactly opposite of what LOS devs did and
    remove LOS health feature from the feature list during system startup
    and finally fix all that mess in runtime.
    
        I SystemServiceManager: Starting org.lineageos.platform.internal.health.HealthInterfaceService
        I servicemanager: Could not find vendor.lineage.health.IChargingControl/default in the VINTF manifest.
        I LineageHealth: Lineage Health HAL not found
    
        E HealthInterface: null health service, SAD!
        D AndroidRuntime: Shutting down VM
        E AndroidRuntime: FATAL EXCEPTION: main
        E AndroidRuntime: Process: org.lineageos.lineageparts, PID: 6073
        E AndroidRuntime: java.lang.RuntimeException: Unable to start receiver org.lineageos.lineageparts.PartsUpdater: java.lang.RuntimeException: Unable to get HealthInterfaceService. The service either crashed, was not started, or the interface has been called too early in SystemServer init
        E AndroidRuntime: 	at android.app.ActivityThread.handleReceiver(ActivityThread.java:4327)
        E AndroidRuntime: 	at android.app.ActivityThread.-$$Nest$mhandleReceiver(Unknown Source:0)
        E AndroidRuntime: 	at android.app.ActivityThread$H.handleMessage(ActivityThread.java:2153)
        E AndroidRuntime: 	at android.os.Handler.dispatchMessage(Handler.java:106)
        E AndroidRuntime: 	at android.os.Looper.loopOnce(Looper.java:201)
        E AndroidRuntime: 	at android.os.Looper.loop(Looper.java:288)
        E AndroidRuntime: 	at android.app.ActivityThread.main(ActivityThread.java:7930)
        E AndroidRuntime: 	at java.lang.reflect.Method.invoke(Native Method)
        E AndroidRuntime: 	at com.android.internal.os.RuntimeInit$MethodAndArgsCaller.run(RuntimeInit.java:548)
        E AndroidRuntime: 	at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:854)
        E AndroidRuntime: Caused by: java.lang.RuntimeException: Unable to get HealthInterfaceService. The service either crashed, was not started, or the interface has been called too early in SystemServer init
        E AndroidRuntime: 	at lineageos.health.HealthInterface.<init>(HealthInterface.java:60)
        E AndroidRuntime: 	at lineageos.health.HealthInterface.getInstance(HealthInterface.java:74)
        E AndroidRuntime: 	at org.lineageos.lineageparts.health.ChargingControlSettings.lambda$static$1(ChargingControlSettings.java:232)
        E AndroidRuntime: 	at org.lineageos.lineageparts.health.ChargingControlSettings.$r8$lambda$G0SM7wLCWwF3KLGg1PRFgibh_kw(Unknown Source:0)
        E AndroidRuntime: 	at org.lineageos.lineageparts.health.ChargingControlSettings$$ExternalSyntheticLambda0.getSummary(Unknown Source:0)
        E AndroidRuntime: 	at org.lineageos.lineageparts.PartsUpdater.fillResultExtras(PartsUpdater.java:91)
        E AndroidRuntime: 	at lineageos.preference.RemotePreferenceUpdater.onReceive(RemotePreferenceUpdater.java:104)
        E AndroidRuntime: 	at android.app.ActivityThread.handleReceiver(ActivityThread.java:4318)
        E AndroidRuntime: 	... 9 more
    
        I am_crash: [6073,0,org.lineageos.lineageparts,952680005,java.lang.RuntimeException,Unable to get HealthInterfaceService. The service either crashed, was not started, or the interface has been called too early in SystemServer init,HealthInterface.java,60]
    
    Signed-off-by: iusmac <iusico.maxim@libero.it>

--- device.mk
+++ device.mk
@@ -196,7 +196,7 @@ PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey
 
 PRODUCT_COPY_FILES += \
-        $(LOCAL_PATH)/configs/gpfspath_oem_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/gpfspath_oem_config.xml
+    $(LOCAL_PATH)/configs/gpfspath_oem_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/gpfspath_oem_config.xml
 
 # Fingerprint
 PRODUCT_PACKAGES += \
@@ -239,6 +239,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.health-service.qti \
     android.hardware.health-service.qti_recovery
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/org.lineageos.health.excluded.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/org.lineageos.health.excluded.xml
 
 # HIDL
 PRODUCT_PACKAGES += \
