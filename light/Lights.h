84b64fe497a2ecaff888377ad522c55dad364108
wudilsr <wudilsr@gmail.com>
2026-04-06 15:45:33 +0800

    m1721: Migrate light HAL from HIDL to AIDL
    Switch from android.hardware.light@2.0 (HIDL) to
    android.hardware.light-V1-ndk (AIDL) for forward compatibility.

---
+++ light/Lights.h
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2021-2022 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#include <aidl/android/hardware/light/BnLights.h>
+#include <mutex>
+#include <vector>
+
+using ::aidl::android::hardware::light::HwLight;
+using ::aidl::android::hardware::light::HwLightState;
+
+namespace aidl {
+namespace android {
+namespace hardware {
+namespace light {
+
+class Lights : public BnLights {
+  public:
+    Lights();
+
+    ndk::ScopedAStatus setLightState(int32_t id, const HwLightState& state) override;
+    ndk::ScopedAStatus getLights(std::vector<HwLight>* _aidl_return) override;
+
+  private:
+    void handleBacklight(uint8_t brightness);
+    void handleNotification(const HwLightState& state);
+
+    std::mutex mLock;
+    HwLightState mBatteryState;
+    HwLightState mNotificationState;
+    std::vector<HwLight> mLights;
+};
+
+} // namespace light
+} // namespace hardware
+} // namespace android
+} // namespace aidl
