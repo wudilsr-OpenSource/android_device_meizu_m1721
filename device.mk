a2b8c079f7aabb2267e949b3a43fce982f8810ca
baunilla <baunilla@tuta.io>
2026-04-07 16:42:28 +0800

    m1721: rootdir: Drop recovery init scripts

--- device.mk
+++ device.mk
@@ -363,8 +363,6 @@ PRODUCT_PACKAGES += \
     init.qcom.sh \
     init.qcom.usb.rc \
     init.qcom.usb.sh \
-    init.recovery.qcom.rc \
-    init.recovery.qcom.usb.rc \
     init.target.rc \
     ueventd.qcom.rc
 
