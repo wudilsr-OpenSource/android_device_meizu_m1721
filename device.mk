a48c7f57c0fddb544084e8feb0f011b6b236e912
Bruno Martins <bgcngm@gmail.com>
2026-04-07 09:38:18 +0800

    m1721: Convert WiFi firmware symlinks to install_symlink targets
    The last bit that was preventing building hermetic images.
    
    Change-Id: I75bb7de4593a7992f6015317cf97e088beeba0cb | AOSP

--- device.mk
+++ device.mk
@@ -469,6 +469,12 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:$(TARGET_COPY_OUT_SYSTEM)/etc/firmware/wlan/prima/WCNSS_cfg.dat \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
+# Wifi firmware symlinks
+PRODUCT_PACKAGES += \
+    firmware_WCNSS_qcom_cfg.ini_symlink \
+    firmware_WCNSS_qcom_wlan_nv.bin_symlink \
+    firmware_WCNSS_wlan_dictionary.dat_symlink
+
 # Custom OTA
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     lineage.updater.uri=https://raw.githubusercontent.com/meizucustoms/OTAUpdates/master/m1721.json
