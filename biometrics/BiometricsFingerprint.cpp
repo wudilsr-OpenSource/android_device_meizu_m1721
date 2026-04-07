bfba660392ed4b1cdaa6b5be5d84f795c7bd9c5a
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2026-04-07 10:54:34 +0800

    m1721: biometrics: Move fingerprint HIDL to 2.3

--- biometrics/BiometricsFingerprint.cpp
+++ biometrics/BiometricsFingerprint.cpp
@@ -13,8 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#define LOG_TAG "android.hardware.biometrics.fingerprint@2.1-service.meizu_m1721"
-#define LOG_WARNING "android.hardware.biometrics.fingerprint@2.1-service.meizu_m1721"
+#define LOG_TAG "android.hardware.biometrics.fingerprint@2.3-service.meizu_m1721"
+#define LOG_WARNING "android.hardware.biometrics.fingerprint@2.3-service.meizu_m1721"
 
 #include <hardware/hw_auth_token.h>
 
@@ -30,7 +30,7 @@ namespace android {
 namespace hardware {
 namespace biometrics {
 namespace fingerprint {
-namespace V2_1 {
+namespace V2_3 {
 namespace implementation {
 
 // Supported fingerprint HAL version
@@ -227,6 +227,19 @@ Return<RequestStatus> BiometricsFingerprint::authenticate(uint64_t operationId,
     return ErrorFilter(mDevice->authenticate(mDevice, operationId, gid));
 }
 
+Return<bool> BiometricsFingerprint::isUdfps(uint32_t /*sensorId*/) {
+    return false;
+}
+
+Return<void> BiometricsFingerprint::onFingerDown(uint32_t /*x*/, uint32_t /*y*/, float /*minor*/,
+                                                 float /*major*/) {
+    return Void();
+}
+
+Return<void> BiometricsFingerprint::onFingerUp() {
+    return Void();
+}
+
 IBiometricsFingerprint* BiometricsFingerprint::getInstance() {
     if (!sInstance) {
       sInstance = new BiometricsFingerprint();
@@ -345,7 +358,7 @@ void BiometricsFingerprint::notify(const fingerprint_msg_t *msg) {
 }
 
 }  // namespace implementation
-}  // namespace V2_1
+}  // namespace V2_3
 }  // namespace fingerprint
 }  // namespace biometrics
 }  // namespace hardware
--- device.mk
+++ device.mk
@@ -200,7 +200,7 @@ PRODUCT_COPY_FILES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.meizu_m1721
+    android.hardware.biometrics.fingerprint@2.3-service.meizu_m1721
 
 # Fwk-detect
 PRODUCT_PACKAGES += \
