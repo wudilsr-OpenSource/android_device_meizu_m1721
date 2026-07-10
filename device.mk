e762def0ce463897570ed15e91e205747e48a735
Yumi Yukimura <me.cafebabe@gmail.com>
2026-07-10 17:39:28 +0800

    m1721: Switch to QTI Memtrack AIDL HAL

--- device.mk
+++ device.mk
@@ -175,15 +175,13 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl-2.1 \
-    android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service \
     gralloc.msm8953 \
     hwcomposer.msm8953 \
     libqdMetaData \
     libtinyxml \
     libvulkan \
-    memtrack.msm8953 \
-    vendor.display.config@1.0.vendor
+    vendor.display.config@1.0.vendor \
+    vendor.qti.hardware.memtrack-service
 
 # Device-specific settings
 PRODUCT_PACKAGES += \
