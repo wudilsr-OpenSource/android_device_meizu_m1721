a2889534f184f2915a6683f7acf23d02c98e6673
Sebastiano Barezzi <seba@sebaubuntu.dev>
2026-07-10 20:46:43 +0800

    m1721: Append _vendor to conflicting vendor libs

--- extract-files.py
+++ extract-files.py
@@ -8,6 +8,10 @@ from extract_utils.fixups_blob import (
     blob_fixup,
     blob_fixups_user_type,
 )
+from extract_utils.fixups_lib import (
+    lib_fixups,
+    lib_fixups_user_type,
+)
 from extract_utils.main import (
     ExtractUtils,
     ExtractUtilsModule,
@@ -21,6 +25,17 @@ namespace_imports = [
     'vendor/qcom/opensource/display',
 ]
 
+def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
+    return f'{lib}_{partition}' if partition == 'vendor' else None
+
+lib_fixups: lib_fixups_user_type = {
+    **lib_fixups,
+    (
+        'com.qualcomm.qti.dpm.api@1.0',
+        'libmmosal',
+        'vendor.qti.imsrtpservice@3.0',
+    ): lib_fixup_vendor_suffix,
+}
 
 # Define the blob fixups
 blob_fixups: blob_fixups_user_type = {
@@ -76,6 +91,7 @@ module = ExtractUtilsModule(
     'm1721',
     'meizu',
     blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
     namespace_imports=namespace_imports,
 )
 
