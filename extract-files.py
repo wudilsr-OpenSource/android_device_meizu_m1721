d6d559722dc30f0d70fb4cb14c75ef88a1a0ec06
ZeeLog <Danjel.Zammit@gmail.com>
2026-07-10 21:42:31 +0800

    m1721: Fix & enable elf checks for libmmcamera-ppeiscore.so * restore GraphicBuffer shim to resolve missing symbol:   _ZN7android13GraphicBufferC1EjjijjP13native_handleb
    * replace libGLESv2 dep with libGLESv2_adreno to resolve missing symbol:
      glGetProgramBinary

--- extract-files.py
+++ extract-files.py
@@ -56,8 +56,9 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('libstdc++.so', 'libstdc++_vendor.so'),
     # Camera - shims, uneeded & VNDK fixups
     'vendor/lib/libmmcamera_ppeiscore.so': blob_fixup()
-        .add_needed('libui_shim.so')
-        .remove_needed('libgui.so'),
+        .add_needed('libppeiscore_shim.so')
+        .remove_needed('libgui.so')
+        .replace_needed('libGLESv2.so', 'libGLESv2_adreno.so'),
     'vendor/lib/libmmcamera_tuning.so': blob_fixup()
         .remove_needed('libmm-qcamera.so'),
     'vendor/lib/libmmcamera2_stats_modules.so': blob_fixup()
