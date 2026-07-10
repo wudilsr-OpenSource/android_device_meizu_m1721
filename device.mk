9dec384c5df809f495ec69a7fd98cdb496b3f514
wudilsr <wudilsr@gmail.com>
2026-07-10 19:10:24 +0800

    m1721: thermal-aidl: Rebrand to meizu_m1721

--- device.mk
+++ device.mk
@@ -400,7 +400,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-service.qti.meizu_m1721
+    android.hardware.thermal-service.qti.meizu_m1721
 
 # Touch features
 PRODUCT_PACKAGES += \
